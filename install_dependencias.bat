3d47eacdef0c5e661b9dedc580aeb1bf9455e962
Esteban <brianmoya777@gmail.com>
2024-11-13 15:08:35 -0500

    CambiosDeFront

---
+++ install_dependencias.bat
@@ -0,0 +1,33 @@
+@echo off
+echo Instalando Flask...
+pip install Flask
+
+echo Instalando Flask-SQLAlchemy...
+pip install Flask-SQLAlchemy
+
+echo Instalando Flask-Login...
+pip install Flask-Login
+
+echo Instalando Werkzeug...
+pip install Werkzeug
+
+echo Instalando pytz...
+pip install pytz
+
+echo Instalando Flask-WTF...
+pip install Flask-WTF
+
+echo Instalando WTForms...
+pip install WTForms
+
+echo Instalando Flask-Mail...
+pip install Flask-Mail
+
+echo Instalando email-validator...
+pip install email-validator
+
+echo Instalación completa de dependencias.
+pause
+
+
+@REM Ejecutar poniendo el nombre del archivo install_dependencias.bat por consola
