44d57652539bb6a83d7ec5fc31b776a61e42dcdf
Esteban <136500531+BEMG1@users.noreply.github.com>
2024-11-13 15:09:11 -0500

    CambiosDeFront (#1)

--- utils/init_bd.py
+++ utils/init_bd.py
@@ -6,7 +6,7 @@ from werkzeug.security import generate_password_hash
 with app.app_context():
     try:
 
-        user1 = User(id=7, email='brianmoya777@gmail.com', username='TestCorreo', password=generate_password_hash('1234'))
+        user1 = User(id=8, email='123@gmail.com', username='admin', password=generate_password_hash('1234'))
         # user2 = User(id=2, username='user', password=generate_password_hash('12345'))
 
 
